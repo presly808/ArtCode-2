e91337c130f65989193ef200bc92ad25a81ed86e
oleksandr.maslovskyj <oleksandr.maslovskyj@worldapp.com>
2016-07-20 10:45:44 +0300

    Task_4_4

---
+++ src/week_2/Task_4_9.java
@@ -0,0 +1,16 @@
+package week_2;
+
+/**
+ * Created by oleksandr.maslovskyj on 20.07.2016.
+ * Задать два массива случайными числами от 25 до 75. Определить у какого из массивов больше парных елементов.
+ */
+public class Task_4_9 {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static void compareArrays(){
+
+    }
+}
