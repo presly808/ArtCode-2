b60479c0d1b302d7cbdf8902cbd0a01565f3a64c
Oleksandr Maslovskij <MaslikSASHA1@gmail.com>
2016-07-24 10:47:20 +0300

    Task_4_4

--- src/week_2_additional/Task_7_2_2.java
+++ src/week_2_additional/Task_7_2_2.java
@@ -1,5 +1,8 @@
 package week_2_additional;
 import week_2.ArrayUtils;
+
+import java.util.Scanner;
+
 /**
  * Created by oleksandr.maslovskyj on 21.07.2016.
  * Проверить или является число простым
@@ -10,23 +13,28 @@ public class Task_7_2_2 {
         simpleValue();
     }
 
-    public static void simpleValue(){
+    public static void simpleValue() {
 
-        int[] array = {1,2,3,4,5,6,7,8,9}; //ArrayUtils.generateArray(10);
-        int counter = 0;
-        System.out.print("array: ");
-        for (int i = 0; i < array.length; i++) {
-            if (array[i] < 2) {
-                array[i] = 0;
-            } else {
-                for (int j = 2; j * j <= array[i]; j++) {
-                    if (array[i] % j == 0) {
-                    } else {
-                        array[i] = 0;
-                    }
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("put value: ");
+        int value = scanner.nextInt();
+
+        if (value <= 2 && value > 0) {
+            System.out.println(value + " :simple");
+        } else {
+            int counter = 0;
+            for (int i = 2; i <= value; i++) {
+                if (value % i == 0) {
+                    counter++;
                 }
             }
-            System.out.print(array[i] + ",");
+
+            if (counter == 1) {
+                System.out.println(value + ": simple");
+            } else {
+                System.out.println( value + ": not simple");
+            }
         }
     }
 }
