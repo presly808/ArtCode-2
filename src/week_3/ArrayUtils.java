b45d5d5a9c6b78bb8c27c72bd6d126f56b04b966
oleksandr.maslovskyj <oleksandr.maslovskyj@worldapp.com>
2016-07-27 19:12:30 +0300

    Task_4_4

---
+++ src/week_3/ArrayUtils.java
@@ -0,0 +1,13 @@
+package week_3;
+
+/**
+ * Created by oleksandr.maslovskyj on 27.07.2016.
+ */
+public class ArrayUtils {
+
+    public static int generateRandomValue(int min, int max){
+        return (int)(Math.random() * (max - min) + min);
+    }
+
+    public static int[] generate
+}
---
+++ src/week_3/Task_3.java
@@ -0,0 +1,21 @@
+package week_3;
+
+/**
+ * Created by oleksandr.maslovskyj on 27.07.2016.
+ * Матрицы генерировать случайными числами и размер матрицы вводит юзер.
+ */
+public class Task_3 {
+    public static void main(String[] args) {
+
+    }
+
+    public static int[][] generateMatrix(int row, int column){
+        int[][] matrix = new int[row][column];
+
+        if (matrix == null) {
+            System.out.println("matrix empty");
+        } else {
+
+        }
+    }
+}
