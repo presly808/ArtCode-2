dcbee5b65aa63e022c7f08e6ee032ec4f6299089
Oleksandr Maslovskij <MaslikSASHA1@gmail.com>
2016-08-03 23:58:51 +0300

    Task_4_4

--- src/week_3/Task_3_8.java
+++ src/week_3/Task_3_8.java
@@ -8,22 +8,25 @@ public class Task_3_8 {
 
     public static void main(String[] args) {
 
+        String fullName = changeToUpperCase("вася пупкин");
+        System.out.println("new name: " + fullName);
     }
 
     public static String[] splitString(String str){
         return str.split(" ");
     }
 
-//    public static String changeToUpperCase(String str){
-//
-//        String[] array = splitString(str);
-//
-//        for (int i = 0; i < array.length; i++) {
-//            if (array[i].length() > 1) {
-//               // array[i].toUpperCase(0);
-//            }
-//        }
-//
-//    }
+    public static String changeToUpperCase(String str){
+
+        String[] array = splitString(str);
+        String fullName = "";
+        for (int i = 0; i < array.length; i++) {
+            if (array[i].length() > 1) {
+                String name = array[i].substring(0, 1).toUpperCase() + array[i].substring(1);
+                fullName += " " + name;
+            }
+        }
+        return fullName.trim();
+    }
 
 }
