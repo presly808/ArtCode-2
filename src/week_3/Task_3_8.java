830a87895315160d595ab32536bc40fd1c23e4c0
oleksandr.maslovskyj <oleksandr.maslovskyj@worldapp.com>
2016-08-03 19:11:25 +0300

    Task_4_4

---
+++ src/week_3/Task_3_8.java
@@ -0,0 +1,29 @@
+package week_3;
+
+/**
+ * Created by oleksandr.maslovskyj on 03.08.2016.
+ * Верхний регистр.
+ */
+public class Task_3_8 {
+
+    public static void main(String[] args) {
+
+    }
+
+    public static String[] splitString(String str){
+        return str.split(" ");
+    }
+
+//    public static String changeToUpperCase(String str){
+//
+//        String[] array = splitString(str);
+//
+//        for (int i = 0; i < array.length; i++) {
+//            if (array[i].length() > 1) {
+//               // array[i].toUpperCase(0);
+//            }
+//        }
+//
+//    }
+
+}
