03f538cc36defa0a2c56be055b12e607e866fbdb
oleksandr.maslovskyj <oleksandr.maslovskyj@worldapp.com>
2016-08-03 18:57:48 +0300

    Task_4_4

--- src/week_3/Task_3_7.java
+++ src/week_3/Task_3_7.java
@@ -5,4 +5,25 @@ package week_3;
  * Найти самое длинное слово в строке(разделенное одним пробелом)
  */
 public class Task_3_7 {
+
+
+    public static void main(String[] args) {
+        String str = findSrtingWithMaxLength("qwert qweqweqwe sdfsdgdfgfdg fdgdfgdfg sdf df dsfdsf");
+        System.out.println("самое длинное слово в строке: " + str);
+    }
+
+    public static String[] splitString(String str){
+        return str.split(" ");
+    }
+
+    public static String findSrtingWithMaxLength(String str) {
+        String[] stringArray = splitString(str);
+        String maxString = "";
+        for (int i = 0; i < stringArray.length; i++) {
+            if (stringArray[i].length() >= maxString.length()) {
+                maxString = stringArray[i];
+            }
+        }
+        return maxString;
+    }
 }
